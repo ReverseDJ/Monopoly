be63e4d4902f58110f3f55d8e767560e973bd062
mtdenonc <mtdenonc@ncsu.edu>
2022-11-30 14:30:02 -0500

    Matt and Ethan
    added function prototypes

--- functions.h
+++ functions.h
@@ -1,1 +1,12 @@
 // functions.h
+#include "classes.h"
+
+//uses players location to determine tile to buy;
+void buyProperty(player * P);
+
+//called if the 
+bool payHow(Player * P);
+
+//takes in a player and a card and returns how many cards the player has that match the type of the passed in card
+int checkMonopoly(Player * P, Card * C);
+
