97ba1b00a015da0802f1f620007238304df63978
eowilson <eowilson@ncsu.edu>
2022-12-05 12:59:23 -0500

    Update functions.h

--- functions.h
+++ functions.h
@@ -4,7 +4,7 @@
 //uses players location to determine tile to buy;
 void buyProperty(player * P);
 
-//called if player needs to pay rent or taxes, needs 
+//called if player needs to pay rent or taxes, needs to check if player has enough money to cover rent value. If not, force to mortgage or initiate bankruptcy
 bool checkBalance(Player * P, int rent);
 
 //takes in a player and a card and returns how many cards the player has that match the type of the passed in card
