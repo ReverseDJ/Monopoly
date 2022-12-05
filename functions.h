8d55189dcd25942eae4b0b1b21b92522b15b8ece
eowilson <eowilson@ncsu.edu>
2022-12-05 12:57:18 -0500

    Update functions.h
    modified checkBalance function to take in rent variable to check

--- functions.h
+++ functions.h
@@ -4,8 +4,8 @@
 //uses players location to determine tile to buy;
 void buyProperty(player * P);
 
-//called if the 
-bool payHow(Player * P);
+//called if player needs to pay rent or taxes, needs 
+bool checkBalance(Player * P, int rent);
 
 //takes in a player and a card and returns how many cards the player has that match the type of the passed in card
 int checkMonopoly(Player * P, Card * C);
