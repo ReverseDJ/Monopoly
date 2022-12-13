4a9ea8137f30933eda67a931a11aacc5d2765751
eowilson <eowilson@ncsu.edu>
2022-12-13 13:53:56 -0500

    Update functions.h
    added optional input to checkBalance

--- functions.h
+++ functions.h
@@ -9,7 +9,7 @@
 void buyProperty(Player * P);
 
 //called if player needs to pay rent or taxes, needs to check if player has enough money to cover rent value. If not, force to mortgage or initiate bankruptcy
-bool checkBalance(Player * P, int money);
+bool checkBalance(Player * P, int money,bool optional);
 
 //takes in a player and a card and returns how many cards the player has that match the type of the passed in card
 int checkMonopoly(Player * P, Card * C);
